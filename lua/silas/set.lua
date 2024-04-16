204451d8b6a5f3783caf269dd498c35a33a31d84
SeeSharpSi <122313938+SeeSharpSi@users.noreply.github.com>
2024-04-16 09:31:18 -0400

    added yank highlighting

--- lua/silas/set.lua
+++ lua/silas/set.lua
@@ -28,3 +28,11 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
